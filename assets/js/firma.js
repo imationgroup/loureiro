bb50650dcc19712accbb92843600c49842ed6d3e
andresalonsomart <andresalonsomart@gmail.com>
2026-09-22 22:17:02 +0200

    Revert "Merge pull request #97 from imationgroup/marca-furgoneta"
    This reverts commit e103a1b8f88c618ded17200398fd8862f0ea86d4, reversing
    changes made to 8bb1a49c5783f754532e4f7e7f6a04104fbd714a.

--- assets/js/firma.js
+++ assets/js/firma.js
@@ -156,7 +156,7 @@ function prepararFirma() {
   ctx.lineWidth = 2.4;
   ctx.lineCap = "round";
   ctx.lineJoin = "round";
-  ctx.strokeStyle = "#1E2533";
+  ctx.strokeStyle = "#14161A";
 
   var pintando = false;
   var punto = function (e) {
